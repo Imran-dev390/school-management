2990f2c097693f9d39980603987a80ae493cb097
imran-k567 <imran@lexumsoft.com>
2025-06-16 20:50:42 +0500

    fixed error

--- Frontend/src/Components/AdminDashboard.jsx
+++ Frontend/src/Components/AdminDashboard.jsx
@@ -62,27 +62,13 @@ if (loading) {
   <h1 className="text-2xl ml-8 sm:text-3xl font-bold">
     🏫 School Admin {name} Dashboard
   </h1>
-  {/* <button
-    onClick={toggleDark}
-    className="flex items-center bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700"
-  >
-    {darkMode ? <FaSun className="mr-2" /> : <FaMoon className="mr-2" />}
-    {darkMode ? 'Light Mode' : 'Dark Mode'}
-  </button> */}
-  {/* <div className="profileShowSchoolName w-fit items-center flex gap-3">
-    <div className="bg bg-white w-14 h-14 rounded-full">
-    </div>
-    <h1>Bright Future</h1>
   </div>
-</div> */}
 <div className="profileShowSchoolName w-fit flex items-center gap-3 p-4">
-  <div className="w-14 h-14 rounded-full flex items-center justify-center bg-transparent text-white">
+  <div className="w-14 h-14 rounded-full flex items-center justify-center bg-transparent">
     <FaUserCircle className="text-4xl" />
   </div>
   <h1 className="text-lg font-semibold text-white">Bright Future</h1>
 </div>
-
-
         {/* Stats */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-10">
           {[
