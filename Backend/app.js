d0f62b6e109f869308a08f24baff013a96ee7aeb
imran-k567 <imran@lexumsoft.com>
2025-06-24 02:36:14 +0500

    100

--- Backend/app.js
+++ Backend/app.js
@@ -111,8 +111,8 @@ db();
 //app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
 // Allow larger JSON and URL-encoded bodies
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(cookieParser());
 app.use(
   cors(corsOptions)
