62dcb1085f3467f589b4de0abd8526a8a3a98139
imran-k567 <imran@lexumsoft.com>
2025-06-24 02:26:53 +0500

    limit 50mb

--- Backend/app.js
+++ Backend/app.js
@@ -108,8 +108,11 @@ app.set("userSocketMap", userSocketMap);
 db();
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+// Allow larger JSON and URL-encoded bodies
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 app.use(
   cors(corsOptions)
