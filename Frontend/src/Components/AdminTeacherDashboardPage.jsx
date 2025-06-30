d7da578b6c926f261599c8d891d6fd4078b97bcb
imran-k567 <imran@lexumsoft.com>
2025-06-30 21:03:27 +0500

    s

--- Frontend/src/Components/AdminTeacherDashboardPage.jsx
+++ Frontend/src/Components/AdminTeacherDashboardPage.jsx
@@ -1,31 +1,32 @@
-import React, { useContext, useEffect, useState } from 'react'
-import AdminLayout from './AdminLayout'
-import { adminDataContext } from '../Context-Api/AdminContext'
-import { authDataContext } from '../Context-Api/AuthContext'
-import { userDataContext } from '../Context-Api/UserContext'
-const AdminTeacherDashboardPage = () => {
-    const {fetchAdminData,adminData} = useContext(adminDataContext);
-    const {sessions = []} = adminData?.admin || [];
-   // const [sessionName,setSessionName] = useState({startDate:"",endDate:""});
+import React, { useContext, useEffect } from 'react';
+import AdminLayout from './AdminLayout';
+import { adminDataContext } from '../Context-Api/AdminContext';
 
-    useEffect(()=>{
-     if(sessions.length > 0){
-        fetchAdminData();
-     }
-    },[fetchAdminData])
+const AdminTeacherDashboardPage = () => {
+  const { fetchAdminData, adminData } = useContext(adminDataContext);
+  const { sessions = [] } = adminData?.admin || {};
+  const currentSession = sessions[0] || {};
+
+  useEffect(() => {
+    fetchAdminData();
+  }, [fetchAdminData]);
+
   return (
-    <AdminLayout adminName='Bright Future'>
-              <main
-  className="flex-1 p-6 pt-20 transition-all duration-300  md:ml-64 ml-0"
->
- <div className="flex flex-col gap-3">
-  <div className="bg-grey headingSchooName">
-<h1>Bright Future Public High School {`${new Date(sessions.startDate.toLocaleDateString() || "2027")} - ${new Date(sessions.endDate.toLocaleDateString() || "2045")}`}</h1>
-  </div>
-      </div>
+    <AdminLayout adminName="Bright Future">
+      <main className="flex-1 p-6 pt-20 transition-all duration-300 md:ml-64 ml-0">
+        <div className="flex flex-col gap-3">
+          <div className="bg-grey headingSchooName">
+            <h1>
+              Bright Future Public High School{' '}
+              {`${currentSession.startDate ? new Date(currentSession.startDate).toLocaleDateString() : '2027'} - ${
+                currentSession.endDate ? new Date(currentSession.endDate).toLocaleDateString() : '2045'
+              }`}
+            </h1>
+          </div>
+        </div>
       </main>
     </AdminLayout>
-  )
-}
+  );
+};
 
-export default AdminTeacherDashboardPage
+export default AdminTeacherDashboardPage;
